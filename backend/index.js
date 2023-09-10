55db826e78a438c522da3797369a358eb08a0276
Doroshkeviches <doroshkeviches@MacBook-Air-Evgenij.local>
2023-09-10 20:07:41 +0300

    feat: add

--- backend/index.js
+++ backend/index.js
@@ -64,6 +64,37 @@ io.on('connection', (socket) => {
       date: Date.now()
     })
   })
+  let id
+
+  // функция принимает `id` адресата, тип события и полезную нагрузку - данные для передачи
+  const emit = (userId, event, data) => {
+    // определяем получателя
+    const receiver = users[userId]
+    if (receiver) {
+      // вызываем событие
+      receiver.emit(event, data)
+    }
+  }
+  socket
+    .on('init', () => {
+      id = nanoid(5)
+      users[id] = socket
+      console.log(id, 'connected')
+      socket.emit('init', { id })
+    })
+    .on('request', (data) => {
+      emit(data.to, 'request', { from: id })
+    })
+    .on('call', (data) => {
+      emit(data.to, 'call', { ...data, from: id })
+    })
+    .on('end', (data) => {
+      emit(data.to, 'end')
+    })
+    .on('disconnect', () => {
+      delete users[id]
+      console.log(id, 'disconnected')
+    })
 })
 
 const start = async () => {
